8dcbd5209a1bc87120d1edad2951ef2eca181d39
tzchao <tzchao.ms02@nctu.edu.tw>
2021-07-12 02:51:11 +0000

    magic chart basics.

--- admin/includes/class-adminMagicCharts.php
+++ admin/includes/class-adminMagicCharts.php
@@ -21,18 +21,56 @@ class VATROC_AdminMagicCharts {
     static public function output() {
         self::$curr_status = new VATROC_CurrStatusTable();
         self::$curr_status->prepare_items( VATROC::$PILOT );
-
+        
+        // wordaround to get jquery-ui css
+        echo '<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">';
         echo "<div class='wrap'>";
-        self::search_form();
+        // self::search_form();
 
         self::suggestion_box();
+        self::selection_box();
 
         echo "<div id='dashboard-widgets' class='metabox-holder'>";
-        self::chart_box( 'https://eaip.caa.gov.tw/eaip/history/2021-07-01/graphics/263172.pdf' );
-        self::chart_box( 'https://www.vatroc.net/' );
+        self::chart_box( 'https://charts.vatroc.net/2021-07-01/263172.pdf-1.jpg' );
+        // self::chart_box( 'https://charts.vatroc.net/2021-07-01/262365.pdf-1.jpg' );
         echo "</div>";
 
         echo "</div>";
+    }
+
+
+    static private function selection_box() {
+        $curl = new WP_Http_Curl();
+        $res = $curl->request( "https://charts.vatroc.net/2021-07-01/charts.csv" );
+        $charts = array(
+            "" => array(),
+            "RCTP" => array(),
+            "RCKH" => array(),
+            "RCSS" => array()
+        );
+        foreach( explode( "\n", $res[ "body" ] ) as $key=>$val ) {
+            $c = explode( ",", $val );
+            if ( !array_key_exists( $c[1], $charts ) ) {
+                $charts[ $c[1] ] = array();
+            } 
+            array_push( $charts[ $c[1] ], [ $c[3], $c[2] ] );
+        }
+        echo '<div class="">';
+        foreach( $charts as $ap=>$_charts ) {
+            usort( $_charts, [ self, 'chart_compare' ] );
+            echo "<select class='toggle-charts'>";
+            echo "<option disabled selected>$ap</option>";
+            foreach( $_charts as $key=>$c ) {
+                echo "<option data-target='$c[0]'>$c[1]</option>";
+            }
+            echo '</select>';
+        }
+        echo '</div>';
+    } 
+
+
+    static private function chart_compare( $e1, $e2 ) {
+        return $e1[1] < $e2[1];
     }
 
 
@@ -51,11 +89,16 @@ class VATROC_AdminMagicCharts {
 
     static private function chart_box( $url ) {
 ?>
-    <div class="postbox-container">
+    <div style="position: absolute; border: 1px dashed #000; overflow: auto; z-index: 9991;" class="postbox-container magic-charts chart-box ui-widget-header">
+        <span class="close-chart" style="position: absolute; right: 0; cursor: pointer;">X</span>
+        <span class="chart-move-up" style="position: absolute; right: 20px; cursor: pointer;">^</span>
+        <span class="chart-move-down" style="position: absolute; right: 40px; cursor: pointer;">v</span>
         <h1>Chart</h1>
-        <iframe src="<?php echo $url; ?>"></iframe>
+        <img style="width: 100%;" src="<?php echo $url; ?>" />
+        <!-- <iframe style="resize: both;" src="<?php echo $url; ?>"></iframe> -->
     </div>
 <?php
+
     }
 
 
@@ -74,6 +117,9 @@ class VATROC_AdminMagicCharts {
 
     public function script_load () {
         wp_enqueue_script( 'magic-charts', plugin_dir_url( VATROC_PLUGIN_FILE ) . '/admin/js/magic_charts.js', array( 'jquery' ), null, true );
+        wp_enqueue_script( 'jquery-ui-core', false, array( 'jquery' ) );
+        wp_enqueue_script( 'jquery-ui-resizable', false, array( 'jquery' ) );
+        wp_enqueue_script( 'jquery-ui-selectmenu', false, array( 'jquery') );
     }
 
 
--- admin/js/magic_charts.js
+++ admin/js/magic_charts.js
@@ -1,3 +1,34 @@
 jQuery(document).ready( ($) => {
-    console.log( "hello world" );
+    function chart_box_handler() {
+        $( ".chart-box" ).draggable();
+        $( ".chart-box" ).resizable();
+        $( ".close-chart" ).on( "click", (e) => {
+            var ele = $(e.target);
+            ele.parent().remove();
+        });
+        $( ".chart-move-up" ).on( "click", (e) => {
+            var ele = $(e.target).parent();
+            ele.css( "z-index", parseInt( ele.css( 'z-index' ) ) + 1 );
+        });
+        $( ".chart-move-down" ).on( "click", (e) => {
+            var ele = $(e.target).parent();
+            ele.css( "z-index", parseInt( ele.css( 'z-index' ) ) - 1 );
+        });
+    }
+
+    chart_box_handler();
+    $( ".toggle-charts" ).selectmenu({
+        select: (e, ui) => {
+            $( "#" + ui.item.element.parent().attr( "id" ) + "-button .ui-selectmenu-text" ).html( ui.item.element.siblings( "option:first" ).text() );
+            var new_chart = $( "<div>", { "class": "postbox-container magic-charts chart-box ui-widget-header", style: "position: absolute; border: 1px dashed #000; overflow: auto; z-index: 9991;" } );
+            new_chart.append( $( "<span>", { text: "X", style: "cursor: pointer; position: absolute; right: 0;", "class": "close-chart" } ) );
+            new_chart.append( $( "<span>", { text: "^", style: "cursor: pointer; position: absolute; right: 20px;", "class": "chart-move-up" } ) );
+            new_chart.append( $( "<span>", { text: "v", style: "cursor: pointer; position: absolute; right: 40px;", "class": "chart-move-down" } ) );
+            new_chart.append( $( "<h1>", { text: "Chart" } ) );
+            new_chart.append( $( "<img>", { style: "width: 100%", src: ui.item.element.data( "target" ) } ) );
+            $( "#dashboard-widgets" ).append( new_chart );
+            chart_box_handler();
+        }
+    });
 });
+
