9c1a535c609f710d75b3fe1303ab8a42b5bdec33
tzchao <tzchao.ms02@nctu.edu.tw>
2022-11-29 15:00:58 +0000

    Added shortcode editable_nickname

--- includes/shortcodes/class-shortcodes-my.php
+++ includes/shortcodes/class-shortcodes-my.php
@@ -11,6 +11,8 @@ class VATROC_Shortcode_My {
 
     public static function init() {
         add_shortcode( 'vatroc_my_vatroc', 'VATROC_Shortcode_My::output_My' );
+        add_shortcode( 'vatroc_my_editable_nickname', 'VATROC_Shortcode_My::editable_nickname' );
+        add_action( "wp_ajax_vatroc_my_set_nickname", "VATROC_Shortcode_My::ajax_set_nickname" );
     }
 
 
@@ -25,6 +27,36 @@ class VATROC_Shortcode_My {
         $ret = "";
         $ret .= "<h1>Applicant section</h1>";
         return $ret;
+    }
+
+
+    public static function editable_nickname() {
+        wp_enqueue_script( 'vatroc-my', plugin_dir_url( VATROC_PLUGIN_FILE ) . 'includes/shortcodes/js/my.js', array( 'jquery' ), null, true );
+
+        ob_start();
+?>
+  <form id="edit-nickname" method="get" action="#">
+  <label for="nickname">顯示名稱
+      <input type="text" id="nickname" name="nickname" value="<?php echo VATROC_Shortcode_My::get_nickname(); ?>" />
+      <button id="submit-nickname" hidden>送出修改</button>
+  </label>
+</form>
+<?php
+        $ret = ob_get_clean();
+        return $ret;
+    }
+
+
+    public static function get_nickname(){
+        return get_userdata( get_current_user_id() )->nickname;
+    }
+
+
+    public static function ajax_set_nickname() {
+        $uid = get_current_user_id();
+        update_user_meta( $uid, "nickname", $_POST[ "nickname"] );
+        echo self::get_nickname();
+        wp_die();
     }
 
 };
