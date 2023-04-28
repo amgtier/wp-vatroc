208b110b70efb0a10484d3268b19b08c1d4f7f71
tzchao <tzchao.ms02@nctu.edu.tw>
2023-04-28 16:39:57 +0000

    [hook] set me up

--- includes/vatroc-hook-functions.php
+++ includes/vatroc-hook-functions.php
@@ -44,6 +44,40 @@ function set_self_applicant() {
 }
 
 
+add_shortcode( 'vatroc_set_me_up_applicant', 'set_me_up_applicant' );
+function set_me_up_applicant() {
+	if ( !is_user_logged_in() ){
+		return do_shortcode();
+	}
+	
+	$ret = "";
+	$uid = get_current_user_ID();
+	$ret .= VATROC_My::html_my_avatar( $uid );
+	if ( VATROC::debug_section() ){
+		return do_shortcode( 'skipped' );
+	}
+
+	if ( VATROC::debug_section( 503 ) ){
+		$ajax_url_unset_me = admin_url( 'admin-ajax.php' ) . '?action=vatroc_unset_me_applicant';
+		$ret .= "<a class='btn btn-default' href='$ajax_url_unset_me'>Unset me</a>";
+	}
+	$new_position = -1;
+	if ( VATROC_My::get_vatroc_position( $uid ) == 0 ){
+		VATROC_My::set_vatroc_position( $uid, $new_position );
+		$ret .= '<div class="btn btn-primary">' . VATROC_Constants::$atc_position[ $new_position ] . ' </div>';
+	}
+	$ret .= "<p>You are set up.</p>";
+
+	return do_shortcode( $ret );
+}
+
+add_action( "wp_ajax_vatroc_unset_me_applicant", "unset_me_applicant" );
+function unset_me_applicant() {
+	VATROC_My::set_vatroc_position( get_current_user_ID(), 0 );
+	wp_redirect( $_SERVER[ HTTP_REFERER ]);
+}
+
+
 add_action( 'wp_enqueue_scripts', 'load_customize_scripts');
 function load_customize_scripts() {
     wp_enqueue_style( 'vatroc-event-calendar', plugin_dir_url( VATROC_PLUGIN_FILE ) . 'includes/css/event-calendar.css' );
